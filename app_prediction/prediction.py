e9d5a6ce6c501251935e6d1a4f346259b32b83e4
irribarre <valdunat@hotmail.com>
2023-08-26 11:15:33 +0200

    création API

---
+++ app_prediction/prediction.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+#import pickle
+import joblib
+
+###################################################
+#             PREDICTION DES DONNEES              #
+###################################################
+
+
+###############################################
+#                  NLTK                       #
+###############################################    
+# Prédiction avec NLTK
+def prediction_nltk(question_in):
+    
+    # Constantes
+#    NLTK_MODEL = 'app_models/model_RandomForestClassifier_NLTK_stemmer.pkl'
+    NLTK_MODEL = 'app_models/model_RandomForestClassifier_NLTK_stemmer.sav'    
+    
+    print("prediction_nltk")
+    
+    # Chargement modèle pré-entraîné avec le modèle USE.
+    # https://medium.com/@maziarizadi/pickle-your-model-in-python-2bbe7dba2bbb
+    # https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
+#    ovr = pickle.load(open(NLTK_MODEL, 'rb'))   
+    ovr = joblib.load(NLTK_MODEL)
+
+    # Making a prediction on the test set
+    y_pred = ovr.predict(question_in)
+    
+    print('y_pred (NLTK) =', y_pred)
+    
+    return(y_pred)
+
+
+
+###############################################
+#                  USE                        #
+###############################################    
+# Prédiction avec USE
+def prediction_use(question_in):
+    
+    # Constantes
+#    USE_MODEL = 'app_models/model_RandomForestClassifier_USE.pkl'
+    USE_MODEL = 'app_models/model_RandomForestClassifier_USE.sav'    
+        
+    print("prediction_use")
+    
+    # Chargement modèle pré-entraîné avec le modèle USE.
+    # https://medium.com/@maziarizadi/pickle-your-model-in-python-2bbe7dba2bbb
+#    ovr = pickle.load(open(USE_MODEL, 'rb'))
+    ovr = joblib.load(NLTK_MODEL)
+    
+    # Making a prediction on the test set
+    y_pred = ovr.predict(question_in)
+    
+    print('y_pred (USE) =', y_pred)
+    
+    return(y_pred)
+ 
