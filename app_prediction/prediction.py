33a583c06276d92c7b06a0f1be496721190aa233
irribarre <valdunat@hotmail.com>
2023-08-26 15:06:50 +0200

    correction joblib --> pickle

--- app_prediction/prediction.py
+++ app_prediction/prediction.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
-#import pickle
-import joblib
+import pickle
+#import joblib
 
 ###################################################
 #             PREDICTION DES DONNEES              #
 ###################################################
+
+C_GOOGLE_DRIVE_PUBLIC__APP_MODELS = 'https://drive.google.com/drive/folders/1tlUxQtNxNlXvcDL7sdJQEuJLv2LfHzbp?usp=drive_link'
 
 
 ###############################################
@@ -15,16 +17,16 @@ import joblib
 def prediction_nltk(question_in):
     
     # Constantes
-#    NLTK_MODEL = 'app_models/model_RandomForestClassifier_NLTK_stemmer.pkl'
-    NLTK_MODEL = 'app_models/model_RandomForestClassifier_NLTK_stemmer.sav'    
+    NLTK_MODEL = 'model_RandomForestClassifier_NLTK_stemmer.pkl'
+#    NLTK_MODEL = 'model_RandomForestClassifier_NLTK_stemmer.sav'    
     
     print("prediction_nltk")
     
     # Chargement modèle pré-entraîné avec le modèle USE.
     # https://medium.com/@maziarizadi/pickle-your-model-in-python-2bbe7dba2bbb
     # https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
-#    ovr = pickle.load(open(NLTK_MODEL, 'rb'))   
-    ovr = joblib.load(NLTK_MODEL)
+    ovr = pickle.load(open(C_GOOGLE_DRIVE_PUBLIC__APP_MODELS + '/' + NLTK_MODEL, 'rb'))   
+#    ovr = joblib.load(NLTK_MODEL)
 
     # Making a prediction on the test set
     y_pred = ovr.predict(question_in)
@@ -42,15 +44,15 @@ def prediction_nltk(question_in):
 def prediction_use(question_in):
     
     # Constantes
-#    USE_MODEL = 'app_models/model_RandomForestClassifier_USE.pkl'
-    USE_MODEL = 'app_models/model_RandomForestClassifier_USE.sav'    
+    USE_MODEL = 'model_RandomForestClassifier_USE.pkl'
+#    USE_MODEL = 'app_models/model_RandomForestClassifier_USE.sav'    
         
     print("prediction_use")
     
     # Chargement modèle pré-entraîné avec le modèle USE.
     # https://medium.com/@maziarizadi/pickle-your-model-in-python-2bbe7dba2bbb
-#    ovr = pickle.load(open(USE_MODEL, 'rb'))
-    ovr = joblib.load(NLTK_MODEL)
+    ovr = pickle.load(open(C_GOOGLE_DRIVE_PUBLIC__APP_MODELS + '/' + USE_MODEL, 'rb'))
+#    ovr = joblib.load(USE_MODEL)
     
     # Making a prediction on the test set
     y_pred = ovr.predict(question_in)
