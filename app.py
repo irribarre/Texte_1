127cb669d774c5eb09a38d8fdaf2913c84320d08
irribarre <valdunat@hotmail.com>
2023-08-29 23:00:41 +0200

    ajour json_example pour mise au point code

--- app.py
+++ app.py
@@ -107,6 +107,42 @@ def query_example():
               <h1>The website value is: {}'''.format(language, framework, website)
 
 
+# GET requests will be blocked
+@app.route('/json-example', methods=['POST'])
+def json_example():
+    request_data = request.get_json()
+
+    language = None
+    framework = None
+    python_version = None
+    example = None
+    boolean_test = None
+
+    if request_data:
+        if 'language' in request_data:
+            language = request_data['language']
+
+        if 'framework' in request_data:
+            framework = request_data['framework']
+
+        if 'version_info' in request_data:
+            if 'python' in request_data['version_info']:
+                python_version = request_data['version_info']['python']
+
+        if 'examples' in request_data:
+            if (type(request_data['examples']) == list) and (len(request_data['examples']) > 0):
+                example = request_data['examples'][0]
+
+        if 'boolean_test' in request_data:
+            boolean_test = request_data['boolean_test']
+
+    return '''
+           The language value is: {}
+           The framework value is: {}
+           The Python version is: {}
+           The item at index 0 in the example list is: {}
+           The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
+
 
 ###############################################
 #                  NLTK                       #
