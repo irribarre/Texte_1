c12df18ec180af6e9ae85fd9b0404fb351c0609a
irribarre <valdunat@hotmail.com>
2023-09-07 22:56:25 +0200

    maj

--- app_prediction/prediction.py
+++ app_prediction/prediction.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import pickle
-from io import StringIO
+
 
 ###################################################
 #             PREDICTION DES DONNEES              #
 ###################################################
-
-#C_GOOGLE_DRIVE_PUBLIC__APP_MODELS = 'https://drive.google.com/drive/folders/1tlUxQtNxNlXvcDL7sdJQEuJLv2LfHzbp?usp=drive_link'
-    
+  
 
     
 ###############################################
@@ -18,64 +16,13 @@ from io import StringIO
 def prediction_nltk(question_in):
     
     print("prediction_nltk, question_in =", question_in)
-        
-    # Constantes
-    # https://stackoverflow.com/questions/64047288/read-a-csv-file-stored-in-google-drive
+            
+    NLTK_MODEL_NAME = './app_models/model_LinearSVC_NLTK_stemmer.pkl'
+    print("prediction_nltk, NLTK_MODEL_NAME =", NLTK_MODEL_NAME)
     
-    nltk_url = 'https://drive.google.com/file/d/1A_GKp8gTMcp9zRa-Zw0Fyjq6A5ECQ2aq/view?usp=drive_link'
     
-#    orig_url = 'https://drive.google.com/file/d/1A_GKp8gTMcp9zRa-Zw0Fyjq6A5ECQ2aq/view?usp=sharing'
-#    file_id  = orig_url.split('/')[-2]
-    file_id  = nltk_url.split('/')[-2]    
-#    dwn_url  = 'https://drive.google.com/uc?export=download&id=' + file_id    
-    dwn_url  = 'https://drive.google.com/uc?id=' + file_id        
-#    NLTK_MODEL_ID = '1A_GKp8gTMcp9zRa-Zw0Fyjq6A5ECQ2aq'
-    NLTK_MODEL_NAME = 'model_RandomForestClassifier_NLTK_stemmer.pkl'
-    
-    print("prediction_nltk, nltk_url =", nltk_url)
-    print("prediction_nltk, file_id =", file_id)
-    print("prediction_nltk, dwn_url =", dwn_url)        
-    
-# https://stackoverflow.com/questions/56611698/pandas-how-to-read-csv-file-from-google-drive-public    
-#url = 'https://drive.google.com/uc?id=0B6GhBwm5vaB2ekdlZW5WZnppb28'
-#dfs = pd.read_csv(url)
-
-#    url = requests.get(dwn_url).text
-#    csv_raw = StringIO(url)
-##    dfs = pd.read_csv(csv_raw)
-
-    # Chargement modèle pré-entraîné avec le modèle USE.
-    # https://medium.com/@maziarizadi/pickle-your-model-in-python-2bbe7dba2bbb
-    # https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
-    # https://stackoverflow.com/questions/56611698/pandas-how-to-read-csv-file-from-google-drive-public    
-    # https://stackoverflow.com/questions/50624042/how-to-unpickle-a-file-that-has-been-hosted-in-a-web-url-in-python
-    
-    # https://realpython.com/urllib-request/
-#    from urllib.request import urlopen
-#    response = urlopen(NLTK_MODEL)
-#    ovr = response.read()
-#    response.close()
-
-#    import cloudpickle as cp
-#    from urllib.request import urlopen
-#    loaded_model = cp.load(urlopen(dwn_url, 'rb'))
-
-#    filename = dwn_url + '/'+ NLTK_MODEL_NAME
-#    filename = dwn_url
-    
-    # Making a prediction on the test set
-    # https://github.com/automl/auto-sklearn/issues/849
-#    pickle.dump(ovr, open(filename, 'wb'))
-
-    loaded_model = pd.read_pickle(r'https://drive.google.com/uc?id=1A_GKp8gTMcp9zRa-Zw0Fyjq6A5ECQ2aq')    
-    
-#    file = open(dwn_url, 'rb')
-#    print('prediction_nltk, filename =', file)
-    
-#    loaded_model = pickle.load(file)
-#    loaded_model = pickle.load(open(filename, 'rb'))
+    loaded_model = pickle.load(open(NLTK_MODEL_NAME, 'rb'))
     y_pred = loaded_model.predict(question_in)
-#    y_pred = ovr.predict(question_in)
     
     print('y_pred (NLTK) =', y_pred)
     
