177ec33ca53a72741e56526d50864a107d8c1032
irribarre <valdunat@hotmail.com>
2023-09-08 15:44:33 +0200

    maj

--- app_prediction/prediction.py
+++ app_prediction/prediction.py
@@ -15,17 +15,40 @@ import pickle
 # Prédiction avec NLTK
 def prediction_nltk(df_question_in):
     
-    print("prediction_nltk, df_question_in =\n", df_question_in)
+    print('prediction_nltk, df_question_in =\n', df_question_in)
             
     NLTK_MODEL_NAME = './app_models/model_LinearSVC_NLTK_stemmer.pkl'
-    print("prediction_nltk, NLTK_MODEL_NAME =", NLTK_MODEL_NAME)
+    print('prediction_nltk, NLTK_MODEL_NAME =', NLTK_MODEL_NAME)
+    
+    
+    # Intégration df_question_in dans les colonnes des données entraînées
+    df_stemmer_columns = pd.read_csv('df_stemmer_columns.csv', sep = '\t')
+    print('prediction_nltk, df_stemmer_columns.shape =', df_stemmer_columns.shape)
+    print('prediction_nltk, df_stemmer_columns =\n', df_stemmer_columns)
+    
+    # Boucle sur les colonnes
+    # https://stacklima.com/boucle-ou-iteration-sur-tout-ou-certaines-colonnes-dun-dataframe-dans-python-pandas/
+    for (column_name, column_data) in df_stemmer_columns.iteritems():
+        if column_name in df_question_in.columns:
+            df_stemmer_columns[column_name] = df_question_in[column_name]
+            print('Column name\t:', column_name)
+            print('Column value\t:', df_stemmer_columns[column_name]) 
+        else:
+            df_stemmer_columns[columnName] = 0
+   
+    # Vérification mise à jour : affichage des colonnes non vides
+    print('prediction_nltk, df_stemmer_columns.shape =', df_stemmer_columns.shape)
+    for (column_name, column_data) in df_stemmer_columns.iteritems():
+        if (columnData.values != 0):
+            print('colonne', column_name, '=', columnData.values)
+        
     
     # Loading model to compare the results
     # https://medium.com/@maziarizadi/pickle-your-model-in-python-2bbe7dba2bbb
     loaded_model = pickle.load(open(NLTK_MODEL_NAME, 'rb'))
-    print("prediction_nltk, loaded_model")
+    print('prediction_nltk, loaded_model')
         
-    y_pred = loaded_model.predict(df_question_in)
+    y_pred = loaded_model.predict(df_question_in !!!!)
     
     print('y_pred (NLTK) =', y_pred)
     
